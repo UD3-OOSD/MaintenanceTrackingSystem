8251ebcdc20d052e47cee2b27da8089d41c2b6f6
nipdep <46063855+nipdep@users.noreply.github.com>
2020-09-30 10:50:55 +0530

    update index.php

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -2,6 +2,7 @@
 
 <?php $this->start('head'); ?>
 <link rel="stylesheet" href="<?=PROOT?>css/home_index.css" media="screen" title="no title" charset="utf-8" >
+<link rel="stylesheet" href="<?=PROOT?>css/extra_css.css" media="screen" title="no title" charset="utf-8" >
 <style>
     body {
         background-image: "<?=PROOT?>app/views/images/Front.jpg";
@@ -13,29 +14,54 @@
 <?php $this->end(); ?>
 
 <?php $this->start('body'); ?>
-<div class="container-fluid" style="">
-  <div class="row">
-    <div class="col-sm-3 col-md-3 col-lg-3">
-      <img id="c001" class="mx-auto d-block" width="90" src="<?=PROOT?>app/views/images/Emblem_of_Sri_Lanka.png">
-      <!-- Original => width="172" height="245" -->
+<div class="container-fluid">
+        <div class="page_header">
+            <div class="row align-items-start">
+                <div class="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
+                    <img id="sl-img" class="mx-auto d-block" width="90" src="sl.png">
+                </div>
+                <div id="sltb" class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                    <div class="row text-center">
+                        <div class="col"></div>
+                        <h3>Sri Lanka Transport Board - Horana Depot</h3>
+                        <div class="col"></div>
+                    </div>
+                    <div class="row text-center">
+                        <div class="col"></div>
+                        <h4>ශ්‍රී ලංකා ගමනාගමන මණ්ඩලය - හොරණ ඩිපෝව</h4>
+                        <div class="col"></div>
+                    </div>
+                    <div class="row text-center">
+                        <div class="col"></div>
+                        <h5>இலங்கை போக்குவரத்து சபை - ஹொரானா டிப்போ</h5>
+                        <div class="col"></div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
+                    <img id="sltb-img" class="mx-auto d-block" width="200" height="120" src="sltb.png">
+                </div>
+            </div>
+        </div>
+        <div class="page_body">
+            <div class="row align-items-center text-center">
+                <div class="col"></div>
+                <div class="col welcome" style="margin-top: 6%; letter-spacing: 6px;">
+                    <h1 class="animate__animated animate__zoomIn animate__delay-0.2s">Welcome!</h1>
+                </div>
+                <div class="col"></div>
+            </div>
+            <div class="row align-items-end justify-content-center">
+                <div style="margin: 10px;">
+                    <a id="btns" class="btn btn-outline-secondary animate__animated animate__bounceInLeft animate__delay-0.5s" href="">Log In</a>
+                </div>
+                <div style="margin: 10px;">
+                    <a id="btns" class="btn btn-outline-secondary animate__animated animate__bounceInLeft animate__delay-0.5" href="">Sign Up</a>
+                </div>
+                <div style="margin: 10px;">
+                    <a id="btns" class="btn btn-outline-secondary animate__animated animate__bounceInLeft animate__delay-0.5s" href="https://www.facebook.com/horana.depot">Visit us on fb</a>
+                </div>
+            </div>
+        </section>
     </div>
-    <div id="c002" class="col-sm-6 col-md-6 col-lg-6">
-      <h3 class="mx-auto d-block">Sri Lanka Transport Board - Horana Depot</h3>
-      <h3 class="mx-auto d-block">ශ්‍රී ලංකා ගමනාගමන මණ්ඩලය - හොරණ ඩිපෝව</h3>
-      <h3 class="mx-auto d-block"> <font size = "5"> இலங்கை போக்குவரத்து சபை - ஹொரானா டிப்போ</h3>
-    </div>
-    <div class="col-sm-3 col-md-3 col-lg-3">
-      <img id="c003" class="mx-auto d-block" width="200" height="120" src="<?=PROOT?>app/views/images/sltb-sri-lanka-transport-board-logo-F90A8F0DBE-seeklogo.png">
-      <!-- Original => width="300" height="151" -->
-    </div>
-  </div>
-  <div class="c004">
-    <h1>Welcome !</h1>
-  </div>
-  <div class="c005">
-    <a class="c006" href="<?=PROOT?>register/login" >Log In</a>
-    <a class="c007" href="https://www.facebook.com/horana.depot" >Visit us on fb</a>
-  </div>
-</div>
 <?php $this->end(); ?>
 
